8b7d5c6c8d0c890babe9692f85ba9f412463ac8b
sourechesathish23 <155233409+sourechesathish23@users.noreply.github.com>
2023-12-30 19:06:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,152 +1,119 @@
 import asyncio
-import requests
-from time import sleep
-from configparser import ConfigParser
-from os import system, name
-from threading import Thread, active_count
-from re import search, compile
+import time
+import importlib
+from pyrogram import *
+from pyrogram.types import *
+from pyrogram.errors import *
+import pyromod
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from aiohttp import web
+routes = web.RouteTableDef()
 
+API_ID = "15037283"
+API_HASH = "7af9d761267bf6b81ed07f942d87127f"
+BOT_TOKEN = "5170036823:AAFBCCvPBUAnI0EnNDlbrLMtSb2sxJtijPg"
 
-THREADS = 500
-PROXIES_TYPES = ('http', 'socks4', 'socks5')
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-REGEX = compile(r"(?:^|\D)?(("+ r"(?:[1-9]|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
-                + r"):" + (r"(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}"
-                + r"|65[0-4]\d{2}|655[0-2]\d|6553[0-5])")
-                + r")(?:\D|$)")
+smtp_server = "smtp-relay.sendinblue.com"
+smtp_port = 587
+smtp_username = "starkpentester@outlook.com"
+smtp_password = "TSrsabGHXhv5npwK"
 
-errors = open('errors.txt', 'a+')
-cfg = ConfigParser(interpolation=None)
-cfg.read("config.ini", encoding="utf-8")
+bot = Client("Stark",api_id=API_ID,api_hash=API_HASH,bot_token=BOT_TOKEN)
 
-http, socks4, socks5 = '', '', ''
-try: http, socks4, socks5 = cfg["HTTP"], cfg["SOCKS4"], cfg["SOCKS5"]
-except KeyError: print(' [ OUTPUT ] Error | config.ini not found!');sleep(3);exit()
-
-http_proxies, socks4_proxies, socks5_proxies = [], [], []
-proxy_errors, token_errors = 0, 0
-channel, post, time_out, real_views = '', 0, 15, 0
-
-def scrap(sources, _proxy_type):
-    for source in sources:
-        if source:
-            try: response = requests.get(source, timeout=time_out)
-            except Exception as e: errors.write(f'{e}\n')
-            if tuple(REGEX.finditer(response.text)):
-                for proxy in tuple(REGEX.finditer(response.text)):
-                    if _proxy_type == 'http': http_proxies.append(proxy.group(1))
-                    elif _proxy_type == 'socks4': socks4_proxies.append(proxy.group(1))
-                    elif _proxy_type == 'socks5': socks5_proxies.append(proxy.group(1))
-
-
-def start_scrap():
-    threads = []
-    for i in (http_proxies, socks4_proxies, socks5_proxies): i.clear()
-    for i in ((http.get("Sources").splitlines(), 'http'), (socks4.get("Sources").splitlines(), 'socks4'), (socks5.get("Sources").splitlines(), 'socks5')):
-        thread = Thread(target=scrap, args=(i[0], i[1]))
-        threads.append(thread)
-        thread.start()
-    for t in threads: t.join()
-
-
-def get_token(proxy, proxy_type):
-    try:
-        session = requests.session()
-        response = session.get(f'https://t.me/{channel}/{post}', params={'embed': '1', 'mode': 'tme'},
-                    headers={'referer': f'https://t.me/{channel}/{post}', 'user-agent': USER_AGENT},
-                    proxies={'http': f'{proxy_type}://{proxy}', 'https': f'{proxy_type}://{proxy}'},
-                    timeout=time_out)
-        return search('data-view="([^"]+)', response.text).group(1), session
-    except AttributeError: return 2
-    except requests.exceptions.RequestException: 1
-    except Exception as e: return errors.write(f'{e}\n')
-
-def send_view(token, session, proxy, proxy_type):
-    try:
-        cookies_dict = session.cookies.get_dict()
-        response = session.get('https://t.me/v/', params={'views': str(token)}, cookies={
-            'stel_dt': '-240', 'stel_web_auth': 'https%3A%2F%2Fweb.telegram.org%2Fz%2F',
-            'stel_ssid': cookies_dict.get('stel_ssid', None), 'stel_on': cookies_dict.get('stel_on', None)},
-                            headers={'referer': f'https://t.me/{channel}/{post}?embed=1&mode=tme',
-                                'user-agent': USER_AGENT, 'x-requested-with': 'XMLHttpRequest'},
-                            proxies={'http': f'{proxy_type}://{proxy}', 'https': f'{proxy_type}://{proxy}'},
-                            timeout=time_out)
-        return True if (response.status_code == 200 and response.text == 'true') else False
-    except requests.exceptions.RequestException: 1
-    except Exception: pass
-
-def control(proxy, proxy_type):
-    global proxy_errors, token_errors
-    token_data = get_token(proxy, proxy_type)
-    if token_data == 2: token_errors += 1
-    elif token_data == 1: proxy_errors += 1
-    elif token_data:
-        send_data = send_view(token_data[0], token_data[1], proxy, proxy_type)
-        if send_data == 1: proxy_errors += 1
-
-
-def start_view():
-    c, threads = 0, []
-    start_scrap()
-    for i in [http_proxies, socks4_proxies, socks5_proxies]:
-        for j in i:
-            thread = Thread(target=control, args=(j, PROXIES_TYPES[c]))
-            threads.append(thread)
-            while active_count() > THREADS: sleep(0.05)
-            thread.start()
-        c += 1
-        sleep(2)
-    for t in threads:
-        t.join()
-        start_view()
-
-
-def check_views():
-    global real_views
-    while True:
-        try:
-            telegram_request = requests.get(f'https://t.me/{channel}/{post}', params={'embed': '1', 'mode': 'tme'},
-                                headers={'referer': f'https://t.me/{channel}/{post}', 'user-agent': USER_AGENT})
-            real_views = search('<span class="tgme_widget_message_views">([^<]+)', telegram_request.text).group(1)
-            sleep(2)
-        except: pass
-
-def tui():
-    while True:
-        print(logo)
-        print(f'''[ ᴅᴀᴛᴀ ]: {channel.capitalize()}/{post}
-[ ʟɪᴠᴇ ᴠɪᴇᴡs ]: {real_views} ✅
- 
-[ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ᴇʀʀᴏʀs ]: {proxy_errors} 🚫
-[ ᴛᴏᴋᴇɴ ᴇʀʀᴏʀs ]: {token_errors} ❌
- 
-[ ᴛʜʀᴇᴀᴅs ]: {active_count()} ⇝⇝⇝⇝ ''')
-        sleep(2)
-
-
-
-
-
-
-
-async def fuck():
-    channel = "marvelcloud"
-    post = "188"
-    try:
-        search('<span class="tgme_widget_message_views">([^<]+)', requests.get(f'https://t.me/{channel}/{post}',
-        params={'embed': '1', 'mode': 'tme'}, headers={'referer': f'https://t.me/{channel}/{post}', 'user-agent': USER_AGENT}).text).group(1)
-    except: print(f'[×] Error | Channel Or Post Not Found!');sleep(3);exit()
+def extract_domain(email):
+    parts = email.split('@')
+    if len(parts) == 2:
+        return parts[1]
     else:
-        print(f'[√]sᴛᴀʀᴛᴇᴅ | ᴡᴀɪᴛ ғᴇᴡ sᴇᴄᴏɴᴅs ᴛᴏ ʀᴜɴ ᴛʜʀᴇᴀᴅs')
-        Thread(target=start_view).start()
-        Thread(target=check_views).start()
-        print("Application Started")
+        return None
 
+def send_email(frommail,tomail,subject,body):
+    try:
+        msg = MIMEMultipart("alternative")
+        msg["From"] = frommail
+        msg["To"] = tomail
+        msg["Subject"] = subject
+        bodys = MIMEText(body)
+        msg.attach(bodys)
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(frommail, tomail, msg.as_string())
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+@bot.on_message(filters.command(["start","new"]) & filters.private)
+async def new(client,message):
+    POLICY = """
+TOS & PP
+
+This Bot is just for educational Purpose only.
+We are not responsible for any illegal Usage or damage by using this bot.
+By using this bot you are agreeing to these terms.
+"""
+    await message.reply(POLICY)
+    email = await message.chat.ask('**Send me your anonymous email name:**', parse_mode=enums.ParseMode.MARKDOWN)
+    data = extract_domain(email.text)
+    if data is not None:
+        await email.request.edit_text(f"**Email received!**\n`{email.text}`")
+    else:
+        return await message.reply("Invalid Email Recived\nPlease try again by /new")
+    frommail = email.text
+    toemail = await message.chat.ask('**Send me your reciver email name:**', parse_mode=enums.ParseMode.MARKDOWN)
+    data = extract_domain(toemail.text)
+    if data is not None:
+        await toemail.request.edit_text(f"**Email received!**\n`{toemail.text}`")
+    else:
+        return await message.reply("Invalid Email Recived\nPlease try again by /new")
+    toemail = toemail.text
+    subject = await message.chat.ask('**Send me the subject of your email:**', parse_mode=enums.ParseMode.MARKDOWN)
+    await subject.request.edit_text(f"**Subject Recived**\n`{subject.text}`")
+    subject = subject.text
+    body = await message.chat.ask("Please send me the body of the email", parse_mode=enums.ParseMode.MARKDOWN)
+    await body.request.edit_text("Body Recived Successfully")
+    text = await message.reply("Sending Data Packs to SMTP Server...")
+    body = body.text
+    is_sent = send_email(frommail,toemail,subject,body)
+    await asyncio.sleep(2)
+    if is_sent==True:
+        TEXT = f"""
+Email Sent Successfully!
+Preview of the Email
+From Email: {frommail}
+To Email: {toemail}
+Subject: {subject}
+Body: {body}
+"""
+        await text.edit(TEXT)
+    else:
+        await text.edit("Unknown Error Occured!")
+
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response({"status": "running"})
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+
+
+async def start_bot():
+    wapp = web.AppRunner(await web_server())
+    await wapp.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(wapp, "0.0.0.0", 5000).start()
+    await bot.start()
+    print("System Started!")
+    await idle()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(fuck())
+    loop.run_until_complete(start_bot())
